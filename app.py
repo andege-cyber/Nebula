1c939f236813534fd746c5eca38e8604cb8c79f5
Pauline Andege <55512611+andege-cyber@users.noreply.github.com>
2024-05-28 23:16:38 +0300

    updates

--- app.py
+++ app.py
@@ -122,9 +122,9 @@ def get_cohort_stats(cohort_name):
 mycursor.close()
 mydb.close()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+ #   app.run(debug=True)
 
 ##########
-#if __name__ == "__main__":
-#    app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
